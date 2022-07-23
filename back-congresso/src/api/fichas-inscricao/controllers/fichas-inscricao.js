53ea65df4de0d9ffdf4b76e69a3aba36f4a423a0
Guilherme Bernardo <bguilherme51@gmail.com>
2022-07-23 17:02:45 -0400

    creating subscription & article tables

--- back-congresso/src/api/fichas-inscricao/controllers/fichas-inscricao.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  fichas-inscricao controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::fichas-inscricao.fichas-inscricao');
--- back-congresso/src/api/fichas-inscricao/services/fichas-inscricao.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fichas-inscricao service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::fichas-inscricao.fichas-inscricao');
