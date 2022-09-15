82d3f046a17d5a014c56262c4001a49cee5aae9f
Guilherme Bernardo <bguilherme51@gmail.com>
2022-09-14 23:48:04 -0400

    update: emailing service midway implemented

--- back-congresso/config/plugins.js
+++ back-congresso/config/plugins.js
@@ -1,15 +1,28 @@
 module.exports = () => ({
-	graphql: {
-	  enabled: true,
-	  config: {
-		endpoint: '/graphql',
-      	shadowCRUD: true,
-		playgroundAlways: true,
-		defaultLimit: 10,
-		maxLimit: 20,
-		apolloServer: {
-		  tracing: true,
-		},
-	  }
-	}
-  })
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "juliasedefdjian@strapi.io",
+        defaultReplyTo: "juliasedefdjian@strapi.io",
+        testAddress: "juliasedefdjian@strapi.io",
+      },
+    },
+  },
+});
