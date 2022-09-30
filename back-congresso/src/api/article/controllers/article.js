d18aee5be1e91eb9d376455afe662037493e0327
Guilherme Bernardo <bguilherme51@gmail.com>
2022-09-29 23:56:03 -0400

    adicionada função de enviar email de confirmação/ WIP

--- back-congresso/src/api/article/controllers/article.js
+++ back-congresso/src/api/article/controllers/article.js
@@ -5,5 +5,17 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({strapi}) => ({
+	async create(ctx){
+		const {data} =	await super.create(ctx);
+		await strapi.plugins['email'].services.email.send({
+			to: 'bguilherme51@gmail.com',
+			from: 'congressorrte@gmail.com',
+			replyTo: 'congressorrte@gmail.com',
+			subject: 'O registro do seu artigo foi realizado com sucesso!!',
+			text: 'Parabens! o seu artigo foi enviado à nossa comissão com as seguintes informações: <br>',
+			html: 'Hello world!',
+		})
+		return {data};
+	}
+}));
