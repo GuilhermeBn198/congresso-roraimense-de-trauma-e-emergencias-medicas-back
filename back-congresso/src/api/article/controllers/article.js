afdd6097a7fa3dff3b00443edcbd2d318b456f83
Guilherme Bernardo <bguilherme51@gmail.com>
2022-10-07 15:20:29 -0400

    Update article.js

--- back-congresso/src/api/article/controllers/article.js
+++ back-congresso/src/api/article/controllers/article.js
@@ -8,13 +8,26 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::article.article', ({strapi}) => ({
 	async create(ctx){
 		const {data} =	await super.create(ctx);
+		console.log(data.attributes.email);
 		await strapi.plugins['email'].services.email.send({
-			to: 'bguilherme51@gmail.com',
+			to: `${data.attributes.email}`,
 			from: 'congressorrte@gmail.com',
 			replyTo: 'congressorrte@gmail.com',
 			subject: 'O registro do seu artigo foi realizado com sucesso!!',
-			text: 'Parabens! o seu artigo foi enviado à nossa comissão com as seguintes informações: <br>',
-			html: 'Hello world!',
+			text: `Parabens! ${data.attributes.nome} o seu artigo ${data.attributes.titulodoartigo} foi enviado à nossa comissão com as seguintes informações:
+			
+			- Titulo: ${data.attributes.titulodoartigo}
+			- Numero: ${data.attributes.numero}
+			- Data de Submissão: ${data.attributes.createdAt}
+			- Modalidade: ${data.attributes.modalidade}
+			- Area Temática: ${data.attributes.areadeestudo}
+			- Autores: ${data.attributes.autores}
+
+			
+	Cordialmente.
+	A Comissão Científica.`,
+
+			html: this.text,
 		})
 		return {data};
 	}
